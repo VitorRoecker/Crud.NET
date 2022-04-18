14091f71bd5bc2cfc6a0012126022d4cf339b8dd
matheus.tambosi <matheus.tambosi@fulltechpro.com.br>
2022-04-18 20:21:39 -0300

    New projects

---
+++ Core/Entidade/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entidade
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public byte Idade { get; set; }
+        public string DocumentoFederal { get; set; }
+    }
+}
---
+++ CrudTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrudTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
