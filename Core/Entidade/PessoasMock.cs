1e14e78dbd83d3d7cb1e066c699cee9785ea4302
matheus.tambosi <matheus.tambosi@fulltechpro.com.br>
2022-04-18 20:34:57 -0300

    Mock e controller

---
+++ Core/Entidade/PessoasMock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entidade
+{
+    public class PessoasMock
+    {
+        public static List<Pessoa> _pessoas = new List<Pessoa>
+        {
+            new Pessoa { Nome = "Matheusin", Idade = 20, DocumentoFederal = "112.214.731-74" },
+            new Pessoa { Nome = "João", Idade = 19, DocumentoFederal = "010.255.002-61" }
+        };
+    }
+}
--- Crud.API/Controllers/WeatherForecastController.cs
+++ Crud.API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using Core.Entidade;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Crud.API.Controllers
 {
@@ -11,29 +8,10 @@ namespace Crud.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public List<Pessoa> GetAll()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return PessoasMock._pessoas;
         }
     }
 }
